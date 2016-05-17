684a8631d82c52e18bb06abedffc65511252760d
diprab <tokushev_madi@mail.ru>
2016-05-18 00:43:31 +0600

    Add files via upload

---
+++ OurViewController3.swift
@@ -0,0 +1,43 @@
+//
+//  OurViewController3.swift
+//  sidebarmenunav
+//
+//  Created by Dina on 11/05/2016.
+//  Copyright (c) 2016 Madi. All rights reserved.
+//
+
+import UIKit
+
+class OurViewController3: UIViewController {
+
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if self.revealViewController() != nil {
+            menuButton.target = self.revealViewController()
+            menuButton.action = "revealToggle:"
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+        
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
